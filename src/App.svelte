1f3d81b0d7f68673ac3ac2fe32c833a379dc86a4
Ankur Dutta <ankur.611@gmail.com>
2024-01-03 18:15:38 +0530

    Pdf viewer working

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Component from "./lib/Component.svelte";
+  import PdfViewer from "./lib/PdfViewer.svelte";
 </script>
 
 <main>
+  <PdfViewer url="./default.pdf" showButtons={[]} showBorder={false} data={null} />
   <Component/>
 </main>
